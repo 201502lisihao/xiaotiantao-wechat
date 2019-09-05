75e90f132940af72e4e74daa294a1b7a8b1ef4b7
201502lisihao <735407073@qq.com>
2019-09-05 09:34:30 +0800

    'update'

--- pages/Order/balance/balance.js
+++ pages/Order/balance/balance.js
@@ -8,34 +8,49 @@ Page({
     cartList: [],
     sumMonney: 0,
     cutMonney: 0,
-    cupNumber: 0
+    cupNumber: 0,
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
+    wx.showLoading({
+      title: '努力加载中',
+    })
+    var that = this;
     wx.setNavigationBarTitle({
       title: '订单详情'
     })
-    this.setData({
+    that.setData({
       cartList: wx.getStorageSync('cartList'),
       sumMonney: wx.getStorageSync('sumMonney'),
       cutMonney: wx.getStorageSync('sumMonney') > 19 ? 3 : 0,
       cupNumber: wx.getStorageSync('cupNumber'),
     })
-
+    //获取可用屏幕高度 55是底部操作框高度
+    var usefulWindowHeight = wx.getSystemInfoSync().windowHeight;
+    that.setData({
+      usefulWindowHeight: usefulWindowHeight - 55
+    });
   },
   gopay: function () {
+    //调微信支付接口，获取支付结果，在成功的回调中跳转下一个页面，保存订单数据，展示下单成功
     wx.navigateTo({
       url: '../detail/detail'
+    })
+  },
+
+  bindTimeChange: function (e) {
+    this.setData({
+      getOrderTime: e.detail.value
     })
   },
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-
+    wx.hideLoading();
   },
 
   /**
@@ -63,7 +78,7 @@ Page({
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-
+    
   },
 
   /**
